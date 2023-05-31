5e93b74a7d7bce09c6d583fd455a969c232e9034
levtus <61996958+levtus@users.noreply.github.com>
2023-05-31 11:49:20 -0500

    Write generalized scene setting functions.

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var sceneNumber;
 
+function slugify(slug) {
+    var rootURL = window.location.protocol + '//' + window.location.hostname;
+    window.history.pushState(null, null, `${rootURL}?${slug}`);
+    return `Address Bar Contents set to ${window.location.href}`
+}
+
 function startGame() {
     hideAll()
+    slugify(game=gaming)
     var currentOpacity = window.getComputedStyle(document.getElementById("lamp")).getPropertyValue("opacity");
     document.documentElement.style.setProperty("--current-opacity", currentOpacity);
     document.getElementById("lamp").style.animation = "disappear 2s linear forwards";
@@ -14,6 +21,49 @@ function startGame() {
 
 function hideAll() {
     document.getElementById("start").style.display='none';
+}
+
+// Generalized Scene function that can be set with parameters to do any scene.
+function scene(num, char1, char2, background, text) {
+    // Set Image for Left Character
+    document.getElementById("char1").src=char1;
+    // If a second character is provided, set that. Else, hide it.
+    if (char2 == null || char2 == "null") {
+        document.getElementById("char2").style.display="none";
+    } else {
+        document.getElementById("char2").src=char2;
+        document.getElementById("char2").style.display="block";
+    }
+    // Set background
+    if (background == null || background == "null") {
+        document.getElementById("background").src="image/background/default.png";
+    } else {
+        document.getElementById("background").src=background;
+    }
+    // Display elements
+    document.getElementById("char1").style.display="block";
+    document.getElementById("background").style.display="block";
+    typeText(text);
+    sceneNumber=num;
+    slugify(`game=gaming&step=${num}`);
+    console.log(`Scene ${num} loaded.`)
+    return num
+}
+
+function setScene(scene) {
+    if (scene == 1) {
+        scene(num, char1, char2, background, text);
+    } else if (scene == 2) {
+    } else if (scene == 3) {
+    } else if (scene == 4) {
+    } else if (scene == 5) {
+    } else if (scene == 6) {
+    } else if (scene == 7) {
+    } else if (scene == 8) {
+    } else if (scene == 9) {
+    } else if (scene == 10) {
+    } else if (scene == 11) {
+    }
 }
 
 function scene1() {
---
+++ slugs.js
@@ -0,0 +1,11 @@
+function checkURL() {
+    const queryString = window.location.search;
+    console.log(`Querystring ${queryString} detected.`);
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.has('game')) {
+        startGame();
+        console.log('Running startGame Function')
+    };
+    const stage = urlParams.get('step');
+    setScene(stage);
+}

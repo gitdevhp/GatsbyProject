c7b5c1663c2522fe765684141e75836e8502de0c
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-06-01 14:17:47 -0500

    display game

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function startGame() {
     var currentOpacity = window.getComputedStyle(document.getElementById("lamp")).getPropertyValue("opacity");
     document.documentElement.style.setProperty("--current-opacity", currentOpacity);
     document.getElementById("lamp").style.animation = "disappear 2s linear forwards";
+    document.getElementById('game').style.display = 'block';
     setScene(1);
 }
 

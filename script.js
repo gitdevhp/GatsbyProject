ae842db1d50ddd2b4f05cb420bee68e0bcb8d8a8
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-06-01 13:08:54 -0500

    poop help

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function scene(num, char1, char2, background, text, option1, option2, option3) {
 //scene setter
 function setScene(scene) {
     if (scene == 1) {
-        scene(scene, 'image/background/default.png', null, background, '["Tom and Daisy arrive to Gatsby\'s party..."]', 'next', null, null);
+        scene(scene, null, null, null, '["Tom and Daisy arrive to Gatsby\'s party..."]', 'next', null, null);
     } else if (scene == 2) {
         scene(scene, char1, char2, background, '[""]');
     } else if (scene == 3) {

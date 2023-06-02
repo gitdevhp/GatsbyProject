df78851564e0d8d8f9f6892b0a80aabb758a2441
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-06-02 14:03:18 -0500

    audio

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var sceneNumber=0;
 var proceed = false;
+const audio = document.querySelector("audio");
 audio.volume = 0.2;
 audio.play();
 //att is mood for green light opactity

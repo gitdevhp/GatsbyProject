0d87cd7282bda196ac5e749cd896f207b37946b0
levtus <61996958+levtus@users.noreply.github.com>
2023-06-03 18:48:54 -0500

    fix audio volume things

--- click.js
+++ click.js
@@ -3,9 +3,13 @@ let kb1 = new Audio('sound/fx/kb_1.mp3');
 let kb2 = new Audio('sound/fx/kb_2.mp3');
 let kb3 = new Audio('sound/fx/kb_3.mp3');
 let kb4 = new Audio('sound/fx/kb_4.mp3');
-for (i = 0; i < 5; i++) {
-    eval('kb' + i.volume = 0.4);
-}
+
+kb0.volume = 0.3
+kb1.volume = 0.3
+kb2.volume = 0.3
+kb3.volume = 0.3
+kb4.volume = 0.3
+
 
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;

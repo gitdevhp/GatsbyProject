fa71c584492a314375e2012b5dcd5c4e4166c303
levtus <61996958+levtus@users.noreply.github.com>
2023-06-05 11:08:24 -0500

    compactify some lines, change some text

--- scenes.js
+++ scenes.js
@@ -16,44 +16,32 @@ function setScene(sceneNum) {
     } else if (sceneNum == 2) {
         console.log('Got to if scene num = 2 statement in setSceneFunction')
         scene(sceneNum, gat_n, placeholder, hall, 
-        '["Nervously, you greet the Buchanan\'s"]', null, 'Talk More with Daisy', 'Look at Daisy but do not talk with her', null);
-    }
-    else if (sceneNum==3) {
-        scene(sceneNum, placeholder, gat_h, hall, '["What a wonderful party you have!"]', 'Daisy', 'Agree and Ask her to dance with you.', null, null);
-    }
-    else if (sceneNum==4) {
-        scene(sceneNum, placeholder, gat_h, hall, '["Yes, I would love to, if you\'ll have me."]', 'Daisy', 'Next', null, null);
-    }
-    else if (sceneNum==5) {
-        scene(sceneNum, placeholder, gat_h, hall, '["As you Dance, you notice a pair of suspicious eyes staring at you and Daisy. It\'s Tom..."]', null, 'next', null, null);
-    }
-    //Path 2 Part 1
-    else if (sceneNum==6) {
+        '["Nervously, you greet the Buchanans"]', null, 'Continue talking with Daisy', 'Look at Daisy, but contain your urge to talk with her', null);
+    } else if (sceneNum==3) {
+        scene(sceneNum, placeholder, gat_h, hall, '["What a lively party you have!"]', 'Daisy', 'Agree apathetically and ask her to dance', null, null);
+    } else if (sceneNum==4) {
+        scene(sceneNum, placeholder, gat_h, hall, '["Yes I would love to, if you\'ll have me."]', 'Daisy', 'Next', null, null);
+    } else if (sceneNum==5) {
+        scene(sceneNum, placeholder, gat_h, hall, '["As you Dance, you notice a pair of eyes glaring suspiciously at you and Daisy."]', null, 'next', null, null);
+    } else if (sceneNum==6) { //Path 2 Part 1
         scene(sceneNum, placeholder, gat_n, hall, 
-            '["You stare at Daisy in awe of her beautiful dress. Tom notices you staring at his wife."]', null, 'Confront Tom', 'Ignore Tom and Lead the Buchanan\'s into the Party', null);
-    }
-    else if (sceneNum==7) {
+            '["You stare at Daisy, in awe of her beautiful dress. Tom notices you staring at his wife."]', null, 'Confront Tom, asking him to express his thoughts', 'Ignore Tom, leading Daisy into the Party', null);
+    } else if (sceneNum==7) {
         scene(sceneNum, tom_n, gat_n, hall, 
-            '["I would never have the likes of you, a bootlegger, be better than me"]', 'Tom', 'Introduce Tom to the groups of people as a Polo Player', null, null);
-    }
-    else if (sceneNum==8) {
+            '["Mind your own business, bootlegger. I don\'t want some lawbreaker messing around in my personal life, and neither does Daisy!"]', 'Tom', 'Introduce Tom to the partygoers as a Polo Player', null, null);
+    } else if (sceneNum==8) {
         scene(sceneNum, gat_h, placeholder, hall, 
-            '["As Tom \"The Polo Player\" awkwardly mingles with party-goers, Daisy starts talking with you."]', null, 'Ask Daisy to dance with you.', 'Wait for Daisy to get comfortable before inviting her to dance', 'Continue to chat with Daisy');
-    }
-    else if (sceneNum==9) {
-        scene(sceneNum, gat_h, null, hall, '["You Continue to keep Daisy company, but you notice that Daisy is getting Bored and Uncomfertable at the Party"]', null, 'Ask Daisy to dance with you.', null, null);
-    }
-    else if (sceneNum==10) {
+            '["As Tom \"The Polo Player\" awkwardly mingles with party-goers, Daisy starts talking you up."]', null, 'Ask Daisy to dance with you.', 'Wait for Daisy to get comfortable before inviting her to dance', 'Continue to chat with Daisy');
+    } else if (sceneNum==9) {
+        scene(sceneNum, gat_h, null, hall, '["You Continue to keep Daisy company, but you notice that Daisy is growing disinterested and wearisome of the party."]', null, 'Ask Daisy to dance with you.', null, null);
+    } else if (sceneNum==10) {
         scene(sceneNum, gat_h, placeholder, hall, 
-            '["Let us Dance! Shall we?"]', 'Daisy', 'Dance Gracefully with Daisy', null, null);
-    }
-    else if (sceneNum==11) {
+            '["Well...alright then Jay. It has been awhile, but I\'d love a spin around the dance floor."]', 'Daisy', 'Dance Gracefully with Daisy', null, null);
+    } else if (sceneNum==11) {
         scene(sceneNum, tom_n, null, hall, 
             '["Tom, suspicous of your relationship with Daisy, follows you in."]', null, 'Introduce Tom as a Polo Player', null, null);
-    }
-    // Part 1 conclusion
-    else if (sceneNum==12) {
+    } else if (sceneNum==12) { // Part 1 conclusion
         scene(sceneNum, placeholder, gat_h, hall, 
-            '["At the end of the Party, Tom is visibly Angry at you as Nick escorts the Buchanan\'s to their departing limo"]', null, 'Approach Nick when he is alone', 'Continue to wave goodbye to leaving Party-Goers', null);
+            '["At the end of the Party, Tom is visibly flustered as the Buchanans are escorted by your neighbor and friend Nick to their waiting limousine."]', null, 'Approach Nick when he is alone', 'Stay where you are, waving goodbye to departing partygoers.', null);
     }
 }

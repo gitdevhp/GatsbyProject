100312443cbde91475f27b727e8571e4afd7e936
levtus <61996958+levtus@users.noreply.github.com>
2023-06-05 17:09:22 -0500

    text formatting changes. buxfixes.

--- scenes.js
+++ scenes.js
@@ -22,7 +22,7 @@ function setScene(sceneNum) {
     } else if (sceneNum == 2) {
         console.log('Got to if scene num = 2 statement in setSceneFunction')
         scene(sceneNum, gat_n, placeholder, hall, 
-        '["Nervously, you greet the Buchanans, welcoming them to the party.\nYour long wait for your love, Daisy Buchanan, to show up to one of your parties has finally ended."]', null, 'Continue talking with Daisy', 'Look at Daisy, but contain your urge to talk with her', null);
+        '["Nervously, you greet the Buchanans, welcoming them to the party. Your long wait for your love, Daisy Buchanan, to show up to one of your parties has finally ended."]', null, 'Continue talking with Daisy', 'Look at Daisy, but contain your urge to talk with her', null);
     } else if (sceneNum==3) {
         scene(sceneNum, placeholder, gat_h, party, 
             '["What a lively party you have!"]', 'Daisy', 'Agree apathetically and ask her to dance', null, null);
@@ -49,34 +49,34 @@ function setScene(sceneNum) {
             '["Well...alright then Jay. It has been awhile, but I\'d love a spin around the dance floor."]', 'Daisy', 'Dance Gracefully with Daisy', null, null);
     } else if (sceneNum==11) {
         scene(sceneNum, tom_n, null, party, 
-            '["Tom, suspicous of your relationship with Daisy, follows you in."]', null, 'Introduce Tom as a Polo Player', null, null);
+            '["Tom, suspicous of your relationship with Daisy, follows you in."]', null, 'Introduce Tom to the partygoers as a Polo Player', null, null);
     } else if (sceneNum==12) { // Part 1 conclusion
         scene(sceneNum, placeholder, gat_h, path, 
-            '["At the end of the Party, Tom is visibly flustered. \nThe Buchanans are escorted by your neighbor and friend Nick to their waiting limousine."]', null, 'Go to your room and approach Nick when he is alone', 'Stay where you are, waving goodbye to departing partygoers.', null);
+            '["By the end of the Party, Tom is visibly flustered. The Buchanans are escorted by your neighbor and friend Nick to their waiting limousine."]', null, 'Go to your room and approach Nick when he is alone', 'Stay where you are, waving goodbye to departing partygoers.', null);
     } else if (sceneNum==13) {
         scene(sceneNum, placeholder, gat_n, hall, 
             '["Chicago is on the line..."]', 'Servant', 'Pick up the phone', 'Tell the servant to explain that you are busy', null,);
     } else if (sceneNum==14) {
         scene(sceneNum, gat_s, null, path,
-            '["You suddenly get sad as you realize that Daisy did not truly enjoy the party"]', null, 'next', null, null,);
+            '["A feeling of sadness sets in as you consider your thoughts that Daisy did not truly enjoy the party"]', null, 'next', null, null,);
     } else if (sceneNum==15) {
         scene(sceneNum, gat_n, null, path, 
-            '["After the phone call, as you stroll outside, the phone call reminds you that the money you\'ve earned has not been earned through rightful means and you hope Daisy never finds out..."]', null, 'next', null, null,);
+            '["Strolling outside, the phone call reminds you of the unrighteous origins of your fortune. You hope Daisy never finds out..."]', null, 'next', null, null,);
     } else if (sceneNum==16) {
         scene(sceneNum, placeholder, gat_s, path, 
-            '["You notice Nick approaching you"]', null, 'Explain to Nick that Daisy did\'nt enojoy the party', null, null,);
+            '["You notice Nick approaching you"]', null, 'Explain to Nick that you don\'t think Daisy enjoyed the party', null, null,);
     } else if (sceneNum==17) {
         scene(sceneNum, gat_s,placeholder, path, 
-            '["I\'m sure she enojoyed it!"]', 'Nick', 'Comment that you feel that your relationship with Daisy is distant', null, null,);
+            '["I\'m sure she enojoyed it! How could she not!"]', 'Nick', 'Comment that you feel that your relationship with Daisy is distant', null, null,);
     } else if (sceneNum==18) {
         scene(sceneNum, gat_s, null, hall, 
-            '["Sadness envelops you as you remember the old Daisy, the kind and understanding Daisy during the War."]', null, 'Beleive that you can fix her...', null, null,);
+            '["Sadness envelops you as you remember the Daisy of your memories: the kind and understanding Daisy."]', null, 'Beleive that you can fix her...', null, null,);
     } else if (sceneNum==19) {
         scene(sceneNum, null, null, null, 
             '["𝘕𝘦𝘹𝘵 𝘋𝘢𝘺"]', null, 'Next', null, null,);
     } else if (sceneNum==20) {
         scene(sceneNum, gat_h, null, hall, 
-            '["Considering Tom\'s behavior yesterday, you begin to grow increasingly worried of being found out.\nIn particular, you worry that your servants will spread word to the press."]', null, 'Relieve them of their responsibilities', 'Ignore the issue and do nothing', null,);
+            '["Considering Tom\'s behavior yesterday, you begin to grow increasingly worried of being found out. In particular, you worry that your servants will spread word to the press."]', null, 'Relieve them of their responsibilities', 'Ignore the issue and do nothing', null,);
     } else if (sceneNum==21) {
         scene(sceneNum, gat_n, wolf, hall, 
             '["You ask your business partner and good friend Meyer Wolfsheim if he knows anyone for your to hire. He suggests a group of young and seemingly cartel-associated workers, ready to work within 48 hours."]', null, 'Hire them for twice the previous servants\' pay rate', null, null,);
@@ -100,9 +100,9 @@ function setScene(sceneNum) {
             '["You arrive at the front step of the Buchanan Estate, the swealtering making you noticably light headed..."]', null, 'Knock on the door, and enter the mansion', null, null,);
     } else if(sceneNum==28) {
         scene(sceneNum, gat_h, daisy, hall, 
-            '["Welcome, Jay!"]', 'Daisy', 'Ask her for a tour of the House', 'Greet her and Look around for Tom', null,);
+            '["Welcome, Jay!"]', 'Daisy', 'Ask Daisy for a tour of the mansion', 'Greet her, before searching around the house for Tom', null,);
     } else if(sceneNum==29){
-        scene(sceneNum, gat_h, null, hall, '["Tom seems to be busy discussing important matters over the phone"]', null, 'next', null, null,);
+        scene(sceneNum, gat_h, null, hall, '["Tom seems to be busy discussing important matters over the phone ..."]', null, 'next', null, null,);
     } else if(sceneNum==30) {
         scene(sceneNum, gat_n, nick, hall, 
             '["Soon, the final guest, Nick, arrives at the house"]', null, 'next', null, null,);

d9efaa86663a3d4b5f9a1c078639448499ec6ca4
levtus <61996958+levtus@users.noreply.github.com>
2023-06-05 14:28:05 -0500

    fufuaug08

--- scenes.js
+++ scenes.js
@@ -66,24 +66,40 @@ function setScene(sceneNum) {
         scene(sceneNum, placeholder, gat_s, path, 
             '["You notice Nick approaching you"]', null, 'Explain to Nick that Daisy did\'nt enojoy the party', null, null,);
     } else if (sceneNum==17) {
-<<<<<<< Updated upstream
         scene(sceneNum, gat_s,placeholder, path, 
-            '["I\'m sure she enojoyed it!"]', 'Nick', 'Continue to whine fr :skull:', null, null,);
+            '["I\'m sure she enojoyed it!"]', 'Nick', 'Comment that you feel that your relationship with Daisy is distant', null, null,);
     } else if (sceneNum==18) {
         scene(sceneNum, gat_s, null, hall, 
             '["Sadness envelops you as you remember the old Daisy, the kind and understanding Daisy during the War."]', null, 'Beleive that you can fix her...', null, null,);
-    } else if(sceneNum==30) {
+    } else if (sceneNum==19) {
+        scene(sceneNum, null, null, null, 
+            '["𝘕𝘦𝘹𝘵 𝘋𝘢𝘺"]', null, 'Next', null, null,);
+    } else if (sceneNum==20) {
+        scene(sceneNum, gat_h, null, hall, 
+            '["Considering Tom\'s behavior yesterday, you begin to grow increasingly worried of being found out.\nIn particular, you worry that your servants will spread word to the press."]', null, 'Relieve them of their responsibilities', 'Ignore the issue and do nothing', null,);
+    } else if (sceneNum==21) {
+        scene(sceneNum, gat_n, wolf, hall, 
+            '["You ask your business partner and good friend Meyer Wolfsheim if he knows anyone for your to hire. He suggests a group of young and seemingly cartel-associated workers, ready to work within 48 hours."]', null, 'Hire them for twice the previous servants\' pay rate', null, null,);
+    } else if (sceneNum==22) {
+        scene(sceneNum, gat_n, wolf, hall, 
+            '["Speaking with Meyer Wolfsheim for unrelated business purposes later that day, he convinces you to hire a group of shady associates that Wolfsheim suggests know how to keep their mouths shut."]', null, 'Hire them for twice the cost of your previous servants', null, null,);
+    } else if (sceneNum==23) {
+        scene(sceneNum, gat_h, null, hall, 
+            '["The next morning, you decide to call your friend Nick to see if he has any advice to offer you when it comes to Daisy."]', null, 'Next', null, null,);
+    } else if (sceneNum==24) {
+        scene(sceneNum, gat_h, nick, hall, 
+            '["Going away? I hear you fired all of your servants."]', 'Nick', 'Reply that you wanted someone who wouldn\'t gossip.', null, null,);
+    } else if (sceneNum==25) {
+        scene(sceneNum, gat_n, nick, hall, 
+            '["I see."]', 'Nick', 'Invite Nick to have lunch with you and Daisy tomorrow afternoon', null, null,);
+    } else if(sceneNum==26) {
         scene(sceneNum, gat_h, tom_n, hall, 
             '["You arrive at the front step of the Buchanan House, the swealtering heat makes you slightly light headed..."]', null, 'Enter the house', null, null,);
-    } else if(sceneNum==31) {
+    } else if(sceneNum==27) {
         scene(sceneNum, gat_h, tom_n, hall, 
             '["Welcome!"]', 'Daisy', 'Ask her for a tour of the House', 'Greet her and Look around for Tom', null,);
-    } else if(sceneNum==32) {
+    } else if(sceneNum==28) {
         scene(sceneNum, gat_h, tom_n, hall, 
-            '["Soon later, the final guest, Nick, arrives at the house"]', null, 'next', null, null,);
-=======
-        scene(sceneNum, gat_h, tom_n, path, 
-            '["I\'m sure she enojoyed it!"]', 'Nick', 'Comment that you feel that your relationship with Daisy is distant.', null, null,);
->>>>>>> Stashed changes
+            '["Soon, the final guest, Nick, arrives at the house"]', null, 'next', null, null,);
     }
 }

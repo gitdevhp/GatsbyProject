c9998f51dd5278d1abbba3b9006fa8be32e4b7ac
levtus <61996958+levtus@users.noreply.github.com>
2023-06-05 21:51:00 -0500

    make the lamp actually look right

--- indicator.js
+++ indicator.js
@@ -1,7 +1,7 @@
 function resize(size) {
     let image = document.getElementById("indicatorImage");
     image.style.width = size + "vh";
-    image.style.width = size + "vh";
+    image.style.height = size + "vh";
 }
 
 function opacity(opacity) {
@@ -11,8 +11,8 @@ function opacity(opacity) {
 }
 
 function updateIndicator(toValue) {
-    opacity((toValue * 0.007 + 0.3).toString())
-    if (toValue > 0.5) {
+    opacity((toValue * 0.006 + 0.2).toString())
+    if (toValue > 50) {
         resize((toValue * 0.2 + 5).toString())
     } else {
         resize('15')

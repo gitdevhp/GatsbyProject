d5f6ddbbc465d1909fb8e9cf8eb8780bd6a6d2de
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-08 08:15:08 -0500

    updated

--- scenes.js
+++ scenes.js
@@ -377,7 +377,7 @@ function setScene(sceneNum) {
         //GUNSHOT SOUNDS BANG BANG BANG 
         //WAIT FOR 3 SECONDS IDK
         //THEN DO A SAD SCENEWHERE GATBY THINKS IF HE COULDVE DONE ANYTHING DIFFERENT
-        typeText('["What couldve been..."]','endtext');
+        typeText('["What could\'ve been...."]','endtext');
         wait(2000);
         // NEXT BUTTON OR OPTION 1 BUTTON THAT GOES TO SCENE 133
         setScene(133);
